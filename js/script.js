c2f8c9ecd3032744f55ae3366c58794acd99b89c
Youreek Cenusa <youreek91@gmail.com>
2020-09-20 22:07:47 +0000

    Changed classes

--- js/script.js
+++ js/script.js
@@ -20,9 +20,9 @@ function showSlides(n) {
   }
 
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" section2-slideshow-dots-dot-active", "");
+      dots[i].className = dots[i].className.replace(" section2__slideshow-dots--dot-active", "");
   }
 
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " section2-slideshow-dots-dot-active";
+  dots[slideIndex-1].className += " section2__slideshow-dots--dot-active";
 }
